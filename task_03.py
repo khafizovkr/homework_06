3d1f181df86f52c8fffb4f0ca150ed6c6d94a1fe
razgildai <hafizov-kamil94@yndex.ru>
2021-02-01 21:08:40 +0300

    2 fix. 3, 4 done

--- task_03.py
+++ task_03.py
@@ -4,3 +4,29 @@
 # реализовать методы получения полного имени сотрудника (get_full_name) и дохода с учетом премии (get_total_income).
 # Проверить работу примера на реальных данных (создать экземпляры класса Position, передать данные, проверить значения
 # атрибутов, вызвать методы экземпляров).
+class Worker:
+    name = None
+    surname = None
+    position = None
+    _income = {"wage": None, "bonus": None}
+
+
+class Position(Worker):
+    def get_full_name(self):
+        print(f'{self.name} {self.surname}')
+
+    def get_total_income(self):
+        print(self._income['wage'] + self._income['bonus'])
+
+
+worker_1 = Position()
+Worker.name = 'Max'
+Worker.surname = 'Boardman'
+Worker.position = 'manager'
+Worker._income = {"wage": 50000, "bonus": 5000}
+print(worker_1.name)
+print(worker_1.surname)
+print(worker_1.position)
+print(worker_1._income)
+worker_1.get_total_income()
+worker_1.get_full_name()
--- task_04.py
+++ task_04.py
@@ -6,3 +6,69 @@
 # скорости.
 # Создайте экземпляры классов, передайте значения атрибутов. Выполните доступ к атрибутам, выведите результат.
 # Выполните вызов методов и также покажите результат.
+class Car:
+    speed = 60
+    color = 'Red'
+    name = None
+    is_police = bool
+
+    def go(self):
+        print(f'{self.name} поехала!')
+
+    def stop(self):
+        print(f'{self.name} остановилась!')
+
+    def turn(self, direction):
+        print(f'{self.name} повернула {direction}')
+
+    def show_speed(self):
+        print(f'Скорость {self.name} {self.speed}')
+
+
+class TownCar(Car):
+    is_police = False
+
+    def show_speed(self):
+        if self.speed > 60:
+            print('Вы превысили скорость')
+        else:
+            print(f'Скорость {self.name} {self.speed}')
+
+
+class SportCar(Car):
+    is_police = False
+
+
+class WorkCar(Car):
+    is_police = False
+
+    def show_speed(self):
+        if self.speed > 40:
+            print('Вы превысили скорость')
+        else:
+            print(f'Скорость {self.name} {self.speed}')
+
+
+class PoliceCar(Car):
+    is_police = True
+
+
+town_car = TownCar()
+TownCar.speed = 80
+TownCar.name = 'Granta'
+TownCar.color = 'Blue'
+sport_car = SportCar()
+SportCar.speed = 100
+SportCar.name = 'Kalina Sport'
+work_car = WorkCar()
+WorkCar.name = 'Largus'
+WorkCar.color = 'White'
+police_car = PoliceCar()
+PoliceCar.name = 'Priora'
+PoliceCar.color = 'white'
+print(work_car.speed)
+work_car.show_speed()
+print(sport_car.color)
+town_car.turn('направо')
+town_car.show_speed()
+police_car.show_speed()
